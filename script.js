4f9d62f813ec1619e76272af280d70600995b329
Mark Hobson <markhobson@gmail.com>
2018-09-11 22:06:46 +0100

    Link to GitHub

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var GammonLayer = L.TileLayer.extend({
 	getTileUrl: function(coords) {
 		var index = Math.ceil(Math.random() * 9);
 		return 'images/gammon' + index + '.jpg';
+	},
+	getAttribution: function() {
+		return '<a href="https://github.com/markhobson/wall-of-gammon">GitHub</a>'
 	}
 });
 
